6b537c0e12e356b158ac22cd3dee439b71820414
Raffy Ramos <raffyramos@Raffys-MacBook-Pro.local>
2024-09-23 17:21:59 +0800

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
+import { socialLinks } from "../data";
+
 const Footer = () => {
   const date = new Date().getFullYear();
   return (
     <section className="footer">
       <p>Let&apos;s build something amazing together</p>
+      <ul className="footer__links">
+        {socialLinks.map((link) => {
+          const { id, href, icon } = link;
+          return (
+            <li key={id}>
+              <a href={href}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
       <span>&#64;{date}</span>
     </section>
   );
