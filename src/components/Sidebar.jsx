11fae352c8eef197e165118c9de7f260dea75594
Raffy Ramos <raffyramos@Raffys-MacBook-Pro.local>
2024-09-29 20:36:29 +0800

    update mouseleave on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,25 @@
 import { links } from "../data";
 import { useGlobalContext } from "../context";
+import { useRef } from "react";
+
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  const sidebarRef = useRef(null);
+
+  const handleMouseLeave = (e) => {
+    const sidebar = sidebarRef.current;
+    const { left, right, bottom } = sidebar.getBoundingClientRect();
+    const { clientX, clientY } = e;
+
+    if (clientX < left - 1 || clientX > right - 1 || clientY > bottom - 1) {
+      closeSidebar();
+    }
+  };
   return (
     <div
       className={isSidebarOpen ? "sidebar sidebar-show" : "sidebar"}
-      onMouseLeave={closeSidebar}
+      ref={sidebarRef}
+      onMouseLeave={handleMouseLeave}
     >
       <ul className="sidebar__links">
         {links.map((link) => {
